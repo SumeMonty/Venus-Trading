7881ae1a4cc9652089579a353a72e27b5b8e9ed1
Sumesh Mohanty <sumemonty@gmail.com>
2023-12-19 18:32:21 +0530

    chg: Created standalone files for PHP components

---
+++ components/footer.php
@@ -0,0 +1,94 @@
+	<!-- FOOTER -->
+	<footer id="footer">
+		<!-- top footer -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-3 col-xs-6">
+						<div class="footer">
+							<h3 class="footer-title">About Us</h3>
+							<p class="footer-description">We procure products in bulk and distribute them to retailers, businesses, or individual
+								customers. We play a crucial role in the supply chain by efficiently managing inventory,
+								and ensuring timely delivery of computer products to meet market demand.</p>
+
+						</div>
+					</div>
+
+					<div class="col-md-3 col-xs-6">
+						<div class="footer">
+							<h3 class="footer-title">Trusted</h3>
+							<h5 class="footer-sub-title">Reviews</h5>
+							<ul class="footer-links">
+								<li><a href="https://g.co/kgs/qaarqH" target="_blank"><i class="fa-brands fa-google"></i>Google</a></li>
+								<li><a href="https://jsdl.in/DT-43BU4TV1" target="_blank">Justdial</a></li>								
+							</ul>
+						</div>
+					</div>
+
+					<div class="clearfix visible-xs"></div>
+
+					<div class="col-md-3 col-xs-6">
+						<div class="footer">
+							<h3 class="footer-title">Information</h3>
+							<ul class="footer-links">
+								<li><a href="#"><i class="fa fa-map-marker"></i>BO - Shop no.4,Bk No.831,Nr A-1 Sweets
+										Kalyan-Ambernath Road,Shivaji Chowk,Ulhasnagar-421003</a></li>
+								<li><i class="fa fa-phone"></i>+91 9850145588</li>
+								<li><a href="mailto:venus1trading@gmail.com">
+									<i class="fa-regular fa-envelope"></i>venus1trading@gmail.com</a>
+								</li>
+								<li><a href="mailto:venustrading@gmail.com">
+									<i class="fa-regular fa-envelope"></i>venustrading@gmail.com</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+
+					<!-- <div class="col-md-3 col-xs-6">
+						<div class="footer">
+							<h3 class="footer-title">Service</h3>
+							<ul class="footer-links">
+								<li><a href="#">My Account</a></li>
+								<li><a href="#">View Cart</a></li>
+								<li><a href="#">Wishlist</a></li>
+								<li><a href="#">Track My Order</a></li>
+								<li><a href="#">Help</a></li>
+							</ul>
+						</div>
+					</div> -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /top footer -->
+
+		<!-- bottom footer -->
+		<div id="bottom-footer" class="section">
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<ul class="footer-payments">
+							<li><a href="#"><i class="fa fa-cc-visa"></i></a></li>
+							<li><a href="#"><i class="fa fa-credit-card"></i></a></li>
+							<li><a href="#"><i class="fa fa-cc-paypal"></i></a></li>
+							<li><a href="#"><i class="fa fa-cc-mastercard"></i></a></li>
+							<li><a href="#"><i class="fa fa-cc-discover"></i></a></li>
+							<li><a href="#"><i class="fa fa-cc-amex"></i></a></li>
+						</ul>
+						<span class="copyright">
+							Copyright &copy;
+							<!-- <script>document.write(new Date().getFullYear());</script> --> All rights reserved
+						</span>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /bottom footer -->
+	</footer>
+	<!-- /FOOTER -->
---
+++ components/header.php
@@ -0,0 +1,129 @@
+<!-- HEADER -->
+<header>
+		<!-- TOP HEADER -->
+		<div id="top-header">
+			<marquee behavior="" direction="">+91 9850145588 | venustrading@gmail.com | Office No. 14, 1st Floor, Babu Building, Opp. Police Station, Lamington Rd, Mumbai - 400 007</marquee>
+			<!-- <div class="container">
+				<ul class="header-links pull-left">
+					<li><a href="#"><i class="fa fa-phone"></i> +91 9850145588</a></li>
+					<li><a href="#"><i class="fa fa-envelope-o"></i> venustrading@gmail.com</a></li>
+					<li><a href="#"><i class="fa fa-map-marker"></i> Shop no.4,Bk No.831,Nr A-1 Sweets
+							Kalyan-Ambernath Road,Shivaji Chowk,Ulhasnagar-421003</a></li>
+				</ul>
+				<ul class="header-links pull-right">
+					<li><a href="#"><i class="fa fa-dollar"></i> USD</a></li>
+					<li><a href="#"><i class="fa fa-user-o"></i> My Account</a></li>
+				</ul>
+			</div> -->
+		</div>
+		<!-- /TOP HEADER -->
+
+		<!-- MAIN HEADER -->
+		<div id="header">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<!-- LOGO -->
+					<div class="col-md-3">
+						<div class="header-logo">
+							<a href="#" class="logo">
+								<img height="20%" width="49%" src="./img/venus-black-filled.png" alt="Venus Trading">
+							</a>
+						</div>
+					</div>
+					<!-- /LOGO -->
+
+					<!-- SEARCH BAR -->
+					<div class="col-md-6">
+						<div class="header-search">
+							<form>
+								<select class="brand-select">
+									<option value="0">All Brands</option>
+									<option value="1">ASUS</option>
+									<option value="1">HP</option>
+									<option value="1">ACER</option>
+									<option value="1">DELL</option>
+								</select>
+								<input class="input" placeholder="Search here">
+								<button class="search-btn">Search</button>
+							</form>
+						</div>
+					</div>
+					<!-- /SEARCH BAR -->
+
+					<!-- ACCOUNT -->
+					<div class="col-md-3 clearfix">
+						<div class="header-ctn">
+							<!-- Wishlist -->
+							<div>
+								<a href="#">
+									<i class="fa-solid fa-heart"></i>
+									<span>Your Wishlist</span>
+									<!-- <div class="qty">2</div> -->
+								</a>
+							</div>
+							<!-- /Wishlist -->
+
+							<!-- Cart -->
+							<div class="dropdown">
+								<a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+									<i class="fa fa-shopping-cart"></i>
+									<span>Your Cart</span>
+									<!-- <div class="qty">3</div> -->
+								</a>
+								<!-- <div class="cart-dropdown">
+									<div class="cart-list">
+										<div class="product-widget">
+											<div class="product-img">
+												<img src="./img/product01.png" alt="">
+											</div>
+											<div class="product-body">
+												<h3 class="product-name"><a href="#">product name goes here</a></h3>
+												<h4 class="product-price"><span class="qty">1x</span>$980.00</h4>
+											</div>
+											<button class="delete"><i class="fa fa-close"></i></button>
+										</div>
+
+										<div class="product-widget">
+											<div class="product-img">
+												<img src="./img/product02.png" alt="">
+											</div>
+											<div class="product-body">
+												<h3 class="product-name"><a href="#">product name goes here</a></h3>
+												<h4 class="product-price"><span class="qty">3x</span>$980.00</h4>
+											</div>
+											<button class="delete"><i class="fa fa-close"></i></button>
+										</div>
+									</div>
+									<div class="cart-summary">
+										<small>3 Item(s) selected</small>
+										<h5>SUBTOTAL: $2940.00</h5>
+									</div>
+									<div class="cart-btns">
+										<a href="#">View Cart</a>
+										<a href="#">Checkout <i class="fa fa-arrow-circle-right"></i></a>
+									</div>
+								</div> -->
+							</div>
+							<!-- /Cart -->
+
+							<!-- Menu Toogle -->
+							<div class="menu-toggle">
+								<a href="#">
+									<i class="fa fa-bars"></i>
+									<span>Menu</span>
+								</a>
+							</div>
+							<!-- /Menu Toogle -->
+						</div>
+					</div>
+					<!-- /ACCOUNT -->
+				</div>
+				<!-- row -->
+			</div>
+			<!-- container -->
+		</div>
+		<!-- /MAIN HEADER -->
+	</header>
+	<!-- /HEADER -->
