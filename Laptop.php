a358c1df303742f18ad875bb77d900ef1e986867
Sumesh Mohanty <sumemonty@gmail.com>
2023-12-23 02:04:42 +0530

    chg: Wrote scripts for laptop page catalogue

--- Laptop.php
+++ Laptop.php
@@ -31,6 +31,7 @@ if (isset($_SESSION['user_id'])) {
 
 	<!-- Bootstrap -->
 	<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
+	<!-- <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" /> -->
 
 	<!-- Slick -->
 	<link type="text/css" rel="stylesheet" href="css/slick.css" />
@@ -57,18 +58,18 @@ if (isset($_SESSION['user_id'])) {
 <body>
 	<!-- HEADER -->
 	<?php
-		include 'components/header.php';
+	include 'components/header.php';
 	?>
 	<!-- /HEADER -->
 
 	<!-- NAVIGATION -->
 	<nav id="navigation">
 		<!-- container -->
-		<div class="container laptop-top-nav">
+		<div class="container laptop-container">
 			<!-- responsive-nav -->
 			<div id="responsive-nav">
 				<!-- NAV -->
-				<ul class="main-nav nav navbar-nav">
+				<ul class="main-nav nav navbar-nav navigator">
 					<li><a href="index.php">Home</a></li>
 					<li class="active"><a href="">Laptop</a></li>
 					<li><a href="contact.php">Contact</a></li>
@@ -76,11 +77,38 @@ if (isset($_SESSION['user_id'])) {
 				<!-- /NAV -->
 			</div>
 			<!-- /responsive-nav -->
-			
+			<!-- responsive-nav -->
+			<div id="responsive-nav-brand">
+				<!-- NAV -->
+				<form method="POST">
+
+					<input type="submit" value="asus" id="asus-btn" name="asus-btn" style="display:none;">
+					<label for="asus-btn" style="cursor: pointer;">
+						<img src="img/asus_logo.png" width="75px" alt="">
+					</label>
+					<input type="submit" value="hp" id="hp-btn" name="hp-btn" style="display:none;">
+					<label for="hp-btn" style="cursor: pointer;">
+						<img src="img/hp_logo.png" width="75px" alt="">
+					</label>
+					<input type="submit" value="acer" id="acer-btn" name="acer-btn" style="display:none;">
+					<label for="acer-btn" style="cursor: pointer;">
+						<img src="img/acer_logo.png" width="70px" alt="">
+					</label>
+					<input type="submit" value="dell" id="dell-btn" name="dell-btn" style="display:none;">
+					<label for="dell-btn" style="cursor: pointer;">
+						<img src="img/dell_logo.png" width="50px" style="margin-left: 10px;" alt="">
+					</label>
+				</form>
+
+				<!-- /NAV -->
+				<!-- </div> -->
+				<!-- /responsive-nav -->
+			</div>
 		</div>
 		<!-- /container -->
 	</nav>
 	<!-- /NAVIGATION -->
+
 
 	<!-- BREADCRUMB -->
 	<div id="breadcrumb" class="lap-breadcrumb" class="section">
@@ -95,7 +123,7 @@ if (isset($_SESSION['user_id'])) {
 						<li class="active">Laptop</li>
 					</ul>
 				</div>
-				
+
 			</div>
 			<!-- /row -->
 			<!-- <div class="logo-navigation">
@@ -112,55 +140,77 @@ if (isset($_SESSION['user_id'])) {
 	<!-- SECTION -->
 	<div class="products laptops">
 		<div class="refinements">
+			<h5 id="clearFilters" style="display: none; margin-top: 2rem; cursor: pointer;">Clear
+				Filters</h5>
 			<div class="brand-refinements">
 				<h5>Brand</h5>
-				<ul class="list-brand">
-					<li>
-						<input class="form-check-input me-1" type="checkbox" name="asus" value="" id="asus">
-						<label class="form-check-label" for="asus">ASUS</label>
-					</li>
-					<li>
-						<input class="form-check-input me-1" type="checkbox" value="" id="hp">
-						<label class="form-check-label" for="hp">HP</label>
-					</li>
-					<li>
-						<input class="form-check-input me-1" type="checkbox" value="" id="acer">
-						<label class="form-check-label" for="acer">ACER</label>
-					</li>
-					<li>
-						<input class="form-check-input me-1" type="checkbox" value="" id="dell">
-						<label class="form-check-label" for="acer">DELL</label>
-					</li>
-				</ul>
+				<form action="">
+					<ul class="list-brand">
+						<li>
+							<input class="form-check-input me-1" type="checkbox" name="asus-ref" value="" id="asusRef"
+								onchange="brand_ref_check()">
+							<label class="form-check-label" for="asus">ASUS</label>
+						</li>
+						<li>
+							<input class="form-check-input me-1" type="checkbox" value="" name="hp-ref" id="hpRef"
+								onchange="brand_ref_check()">
+							<label class="form-check-label" for="hp">HP</label>
+						</li>
+						<li>
+							<input class="form-check-input me-1" type="checkbox" value="" name="acer-ref" id="acerRef"
+								onchange="brand_ref_check()">
+							<label class="form-check-label" for="acer">ACER</label>
+						</li>
+						<li>
+							<input class="form-check-input me-1" type="checkbox" value="" name="dell-ref" id="dellRef"
+								onchange="brand_ref_check()">
+							<label class="form-check-label" for="acer">DELL</label>
+						</li>
+						<!-- <li>
+							<label class="form-check-label" id="clearBrand"
+								style="display: none; cursor: pointer;">Clear All</label>
+						</li> -->
+					</ul>
+				</form>
 
 			</div>
 			<div class="ram-size-refinements">
 				<h5>RAM</h5>
 				<ul class="list-ram-size">
 					<li>
-						<input class="form-check-input me-1" type="checkbox" value="" id="4gb">
+						<input class="form-check-input me-1" type="checkbox" value="" id="fourGBRef"
+							onchange="ram_ref_check()">
 						<label class="form-check-label" for="4gb">4 GB</label>
 					</li>
 					<li>
-						<input class="form-check-input me-1" type="checkbox" value="" id=" 8gb">
+						<input class="form-check-input me-1" type="checkbox" value="" id="eightGBRef"
+							onchange="ram_ref_check()">
 						<label class="form-check-label" for="8gb">8 GB</label>
 					</li>
 					<li>
-						<input class="form-check-input me-1" type="checkbox" value="" id="16gb">
+						<input class="form-check-input me-1" type="checkbox" value="" id="steenGBRef"
+							onchange="ram_ref_check()">
 						<label class="form-check-label" for="16gb">16 GB</label>
 					</li>
 					<li>
-						<input class="form-check-input me-1" type="checkbox" value="" id="32gb">
+						<input class="form-check-input me-1" type="checkbox" value="" id="ttwoGBRef"
+							onchange="ram_ref_check()">
 						<label class="form-check-label" for="32gb">32 GB</label>
 					</li>
 					<li>
-						<input class="form-check-input me-1" type="checkbox" value="" id="64gb">
+						<input class="form-check-input me-1" type="checkbox" value="" id="sfourGBRef"
+							onchange="ram_ref_check()">
 						<label class="form-check-label" for="64gb">64 GB</label>
 					</li>
+					<!-- <li>
+						<label class="form-check-label" id="clearRam" style="display: none; cursor: pointer;">Clear
+							All</label>
+					</li> -->
 				</ul>
 
+
 			</div>
-			<div class="ram-tech-refinements">
+			<!-- <div class="ram-tech-refinements">
 				<h5>RAM Memory Technology</h5>
 				<ul class="list-ram-tech">
 					<li>
@@ -177,8 +227,8 @@ if (isset($_SESSION['user_id'])) {
 					</li>
 				</ul>
 
-			</div>
-			<div class="disk-refinements">
+			</div> -->
+			<!-- <div class="disk-refinements">
 				<h5>Hard Disk Type</h5>
 				<ul class="list-ram-tech">
 					<li>
@@ -192,69 +242,137 @@ if (isset($_SESSION['user_id'])) {
 
 				</ul>
 
-			</div>
+			</div> -->
 		</div>
-		<!-- container -->
+		<button type="button" class="btn btn-primary responsive-filter" data-toggle="button" aria-pressed="false"
+			autocomplete="off" onclick="toggleref()">
+			<i class="fa-solid fa-arrow-right"></i>
+		</button>
+
+		<!-- <div class="andagundu"> -->
 		<div class="box-container">
 
 
 			<?php
-			$sql_query = "SELECT * FROM `products`";
+			$sql_query = "SELECT id, SUBSTRING(description, 1, 20) AS shortdesc, description, brand, price, img, ram FROM `products`";
+
+			// Refinements Start
+			$brand_string = '';
+			$ram_string = '';
+			if (isset($_GET['brand'])) {
+				$brand_string = implode(',', $_GET['brand']);
+				if ($ram_string != '') {
+					$sql_query .= ' AND brand IN($brand_string)';
+
+				} else {
+					$sql_query = "SELECT id, SUBSTRING(description, 1, 20) AS shortdesc, description, brand, price, img, ram FROM `products` WHERE brand IN($brand_string)";
+				}
+				// echo $sql_query;
+				// run_my_query($conn, $sql_query);
+			} else {
+				// echo 'URL parameter does not exist';
+				// run_my_query($conn, $sql_query);
+			}
+
+
+
+			if (isset($_GET['ram'])) {
+				$ram_string = implode(',', $_GET['ram']);
+				// echo $ram_string;
+				if ($brand_string != '') {
+					$sql_query = $sql_query . " AND ram IN($ram_string)";
+				} else {
+					$sql_query = "SELECT id, SUBSTRING(description, 1, 20) AS shortdesc, description, brand, price, img, ram FROM `products` WHERE ram IN
+				($ram_string)";
+				}
+				// run_my_query($conn, $sql_query);
+			} else {
+				// echo 'URL parameter does not exist';
+				// run_my_query($conn, $sql_query);
+			}
+			// Refinements End
 			
+			// Brand Filters Start
+			if (isset($_POST['asus-btn'])) {
+				// echo $_POST['asus-btn']	;
+				$sql_query = $sql_query . "WHERE brand IN('ASUS')";
+				run_my_query($conn, $sql_query);
+			}
+			if (isset($_POST['hp-btn'])) {
+				// echo $_POST['hp-btn']	;
+				$sql_query = $sql_query . "WHERE brand IN('HP')";
+				run_my_query($conn, $sql_query);
+			}
+			if (isset($_POST['acer-btn'])) {
+				// echo $_POST['acer-btn']	;
+				$sql_query = $sql_query . "WHERE brand IN('ACER')";
+				run_my_query($conn, $sql_query);
+			}
+			if (isset($_POST['dell-btn'])) {
+				$sql_query = $sql_query . "WHERE brand IN('DELL')";
+				run_my_query($conn, $sql_query);
+			}
+			// Brnd Filters End
+			
+
+
 			if (isset($_POST['search-btn'])) {
 				$searchedItem = $_POST['search-value'];
 				$selectedBrand = $_POST['brand-select'];
-				$sql_query = $sql_query . " WHERE description LIKE '%$searchedItem%'";
-				if($selectedBrand != "ALL") {
+				$sql_query = "SELECT id, SUBSTRING(description, 1, 20) AS shortdesc, description, brand, price, img, ram FROM `products` WHERE description LIKE '%$searchedItem%'";
+				if ($selectedBrand != "ALL") {
 					$sql_query = $sql_query . " AND brand = '$selectedBrand'";
 					// echo $sql_query;
 				}
+				// run_my_query($conn, $sql_query);
 			}
-			
-			$select_products = $conn->prepare($sql_query);
-			$select_products->execute();
 
-			if ($select_products->rowCount() > 0) {
-				while ($fetch_product = $select_products->fetch(PDO::FETCH_ASSOC)) {
-					?>
-					<!-- product -->
-					<div class="product" data-brand="<?= $fetch_product['brand']; ?>">
-						<p hidden class="product-brand">
-							<?= $fetch_product['brand']; ?>
-						</p>
-						<div class="product-img">
-							<img width="100%" height="50%" src="./img/<?= $fetch_product['img']; ?>.jpg" alt="">
-						</div>
-						<div class="product-body">
-							<h3 class="product-name"><a href="#">
-									<?= $fetch_product['brand']; ?>
-									&nbsp;<?= $fetch_product['description']; ?>
-								</a></h3>
-							<h4 class="product-price">₹
-								<?= $fetch_product['price']; ?>
-							</h4>
-							<div class="product-btns">
-								<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to
-										wishlist</span></button>
-								<button class="add-to-compare"><a href="https://wa.me/9137503778?text=I'm%20interested%20in%20buying%20<?= $fetch_product['brand']; ?>%20<?= $fetch_product['description']; ?>"><i class="fa fa-shopping-cart"></i><span class="tooltipp">Buy Now</span></a></button>
-								<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick
-										view</span></button>
+			// echo "Query: " . $sql_query;
+			run_my_query($conn, $sql_query);
+
+			function run_my_query($conn, $sql_query)
+			{
+				$select_products = $conn->prepare($sql_query);
+				$select_products->execute();
+				if ($select_products->rowCount() > 0) {
+					while ($fetch_product = $select_products->fetch(PDO::FETCH_ASSOC)) {
+						?>
+						<form action="" method="post" class="box">
+							<input type="hidden" name="pid" value="<?= $fetch_product['id']; ?>">
+							<input type="hidden" name="name"
+								value="<?= $fetch_product['brand']; ?> <?= $fetch_product['shortdesc']; ?>">
+							<input type="hidden" name="price" value="<?= $fetch_product['price']; ?>">
+							<input type="hidden" name="image" value="<?= $fetch_product['img']; ?>">
+							<button class="fas fa-heart" type="submit" name="add_to_wishlist"></button>
+							<a href="quick-view.php?pid=<?= $fetch_product['id']; ?>" class="fas fa-eye"></a>
+							<img src="img/<?= $fetch_product['img']; ?>.jpg" alt="">
+							<div class="name">
+								<?= $fetch_product['brand']; ?>
+								<?= $fetch_product['description']; ?>
 							</div>
-						</div>
-						<div class="add-to-cart">
-							<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-						</div>
-					</div>
-					<!-- /product -->
-
-					<?php
+							<div class="flex">
+								<div class="price"><span>₹</span>
+									<?= $fetch_product['price']; ?><span>/-</span>
+								</div>
+								<input type="number" name="qty" class="qty" min="1" max="99"
+									onkeypress="if(this.value.length == 2) return false;" value="1">
+							</div>
+							<button type="submit" class="btn" name="buy-now">
+								<a href="https://wa.me/9137503778?text=I'm%20interested%20in%20buying%20<?= $fetch_product['brand']; ?>%20<?= $fetch_product['description']; ?>"
+									target="_blank">Buy
+									Now</a>
+							</button>
+						</form>
+						<?php
+					}
+				} else {
+					echo '<p class="empty">no products found!</p>';
 				}
-			} else {
-				echo '<p class="empty">no products found!</p>';
 			}
 			?>
 
 		</div>
+		<!-- </div> -->
 		<!-- /row -->
 	</div>
 	<!-- /container -->
@@ -264,11 +382,13 @@ if (isset($_SESSION['user_id'])) {
 	<!-- FOOTER -->
 	<?php
 	include 'components/footer.php'
-	?>
+		?>
 	<!-- /FOOTER -->
 
 	<!-- jQuery Plugins -->
-	<script src="js/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+		integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+		crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<script src="js/slick.min.js"></script>
 	<script src="js/nouislider.min.js"></script>
--- js/main.js
+++ js/main.js
@@ -125,16 +125,16 @@
 		})
 	});
 
-	var priceInputMax = document.getElementById('price-max'),
-			priceInputMin = document.getElementById('price-min');
+	// var priceInputMax = document.getElementById('price-max'),
+	// 		priceInputMin = document.getElementById('price-min');
 
-	priceInputMax.addEventListener('change', function(){
-		updatePriceSlider($(this).parent() , this.value)
-	});
+	// priceInputMax.addEventListener('change', function(){
+	// 	updatePriceSlider($(this).parent() , this.value)
+	// });
 
-	priceInputMin.addEventListener('change', function(){
-		updatePriceSlider($(this).parent() , this.value)
-	});
+	// priceInputMin.addEventListener('change', function(){
+	// 	updatePriceSlider($(this).parent() , this.value)
+	// });
 
 	function updatePriceSlider(elem , value) {
 		if ( elem.hasClass('price-min') ) {
@@ -166,3 +166,257 @@
 	}
 
 })(jQuery);
+
+var locationHref = 'Laptop.php?';
+
+function brand_ref_check() {
+	
+	var brand_ref = [];
+	var asus = document.querySelector('#asusRef');
+	var hp = document.querySelector('#hpRef');
+	var acer = document.querySelector('#acerRef');
+	var dell = document.querySelector('#dellRef');
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+
+
+	if(asus.checked != false) {
+		// brand_ref.push('asus');
+		if(urlParams.has('brand[]')) {
+			for(const [key, value] of urlParams.entries()) {
+				if(value != '\'asus\'') window.location.search+='brand[]=\'asus\'&';
+			}
+		} else {
+			window.location.search+='brand[]=\'asus\'&';
+		}
+		
+		
+		document.querySelector('#clearFilters').style.display = "block";
+	}
+	if(hp.checked != false) {
+		// brand_ref.push('hp');
+		if(urlParams.has('brand[]')) {
+			for(const [key, value] of urlParams.entries()) {
+				if(value != '\'hp\'') window.location.search+='brand[]=\'hp\'&';
+			}
+		} else {
+			window.location.search+='brand[]=\'hp\'&';
+		}
+		document.querySelector('#clearFilters').style.display = "block";
+	}
+	if(acer.checked != false) {
+		// brand_ref.push('acer');
+		if(urlParams.has('brand[]')) {
+			for(const [key, value] of urlParams.entries()) {
+				if(value != '\'acer\'') window.location.search+='brand[]=\'acer\'&';
+			}
+		} else {
+			window.location.search+='brand[]=\'acer\'&';
+		}
+		document.querySelector('#clearFilters').style.display = "block";
+	}
+	if(dell.checked != false) {
+		// brand_ref.push('dell');
+		if(urlParams.has('brand[]')) {
+			for(const [key, value] of urlParams.entries()) {
+				if(value != '\'dell\'') window.location.search+='brand[]=\'dell\'&';
+			}
+		} else {
+			window.location.search+='brand[]=\'dell\'&';
+		}
+		document.querySelector('#clearFilters').style.display = "block";
+	}
+	if(urlParams.has('ram[]')) {
+		locationHref = 'Laptop.php' + window.location.search;
+	}
+	// brand_ref.forEach(brand => {
+	// 	locationHref = locationHref + `brand[]='${brand}'&`;
+	// });
+	// window.location.href = locationHref;
+	
+}
+
+// RAM REF CHECK
+
+
+function ram_ref_check() {
+	
+	var ram_ref = [];
+	var fourGB = document.querySelector('#fourGBRef');
+	var eightGB = document.querySelector('#eightGBRef');
+	var steenGB = document.querySelector('#steenGBRef');
+	var ttwoGB = document.querySelector('#ttwoGBRef');
+	var sfourGB = document.querySelector('#sfourGBRef');
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+
+	if(fourGB.checked != false) {
+		// ram_ref.push('4gb');
+		if(urlParams.has('ram[]')) {
+			for(const [key, value] of urlParams.entries()) {
+				if(value != '\'4gb\'') window.location.search+='ram[]=\'4gb\'&';
+			}
+		} else {
+			window.location.search+='ram[]=\'4gb\'&';
+		}
+		document.querySelector('#clearFilters').style.display = "block";
+	}
+	if(eightGB.checked != false) {
+		// ram_ref.push('8gb');
+		if(urlParams.has('ram[]')) {
+			for(const [key, value] of urlParams.entries()) {
+				if(value != '\'8gb\'') window.location.search+='ram[]=\'8gb\'&';
+			}
+		} else {
+			window.location.search+='ram[]=\'8gb\'&';
+		}
+		document.querySelector('#clearFilters').style.display = "block";
+	}
+	if(steenGB.checked != false) {
+		// ram_ref.push('16gb');
+		if(urlParams.has('ram[]')) {
+			for(const [key, value] of urlParams.entries()) {
+				if(value != '\'16gb\'') window.location.search+='ram[]=\'16gb\'&';
+			}
+		} else {
+			window.location.search+='ram[]=\'16gb\'&';
+		}
+		document.querySelector('#clearFilters').style.display = "block";
+	}
+	if(ttwoGB.checked != false) {
+		// ram_ref.push('32gb');
+		if(urlParams.has('ram[]')) {
+			for(const [key, value] of urlParams.entries()) {
+				if(value != '\'32gb\'') window.location.search+='ram[]=\'32gb\'&';
+			}
+		} else {
+			window.location.search+='ram[]=\'32gb\'&';
+		}
+		document.querySelector('#clearFilters').style.display = "block";
+	}
+	
+	if(sfourGB.checked != false) {
+		// ram_ref.push('64gb');
+		if(urlParams.has('ram[]')) {
+			for(const [key, value] of urlParams.entries()) {
+				if(value != '\'64gb\'') window.location.search+='ram[]=\'64gb\'&';
+			}
+		} else {
+			window.location.search+='ram[]=\'64gb\'&';
+		}
+		document.querySelector('#clearFilters').style.display = "block";
+	}
+	
+	
+	if(urlParams.has('brand[]')) {
+		locationHref = 'Laptop.php' + window.location.search;
+	}
+	// ram_ref.forEach(ram => {
+	// 	locationHref = locationHref + `ram[]='${ram}'&`;
+	// });
+	// window.location.href = locationHref;
+	
+}
+
+// RAM REF CHECK
+
+document.addEventListener("DOMContentLoaded", retainChecked());
+
+function retainChecked() {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	
+	// Brand Start
+
+	const brand = urlParams.getAll('brand[]');
+	brand.forEach(el => {
+		if(el === '\'asus\'') {
+			document.querySelector('#asusRef').checked = true
+			document.querySelector('#clearFilters').style.display = "block";
+		}
+		if(el === '\'hp\'') {
+			document.querySelector('#hpRef').checked = true
+			document.querySelector('#clearFilters').style.display = "block";
+		}
+		if(el === '\'acer\'') {
+			document.querySelector('#acerRef').checked = true
+			document.querySelector('#clearFilters').style.display = "block";
+		}
+		if(el === '\'dell\'') {
+			document.querySelector('#dellRef').checked = true
+			document.querySelector('#clearFilters').style.display = "block";
+		}
+	});
+
+	// Brand End
+
+
+	// Ram Start
+
+	const ram = urlParams.getAll('ram[]');
+	ram.forEach(el => {
+		if(el === '\'4gb\'') {
+			document.querySelector('#fourGBRef').checked = true
+			document.querySelector('#clearFilters').style.display = "block";
+		}
+		if(el === '\'8gb\'') {
+			document.querySelector('#eightGBRef').checked = true
+			document.querySelector('#clearFilters').style.display = "block";
+		}
+		if(el === '\'16gb\'') {
+			document.querySelector('#steenGBRef').checked = true
+			document.querySelector('#clearFilters').style.display = "block";
+		}
+		if(el === '\'32gb\'') {
+			document.querySelector('#ttwoGBRef').checked = true
+			document.querySelector('#clearFilters').style.display = "block";
+		}
+		if(el === '\'64gb\'') {
+			document.querySelector('#sfourGBRef').checked = true
+			document.querySelector('#clearFilters').style.display = "block";
+		}
+	});
+
+	// Ram End
+
+}
+
+document.querySelector('#clearFilters').addEventListener('click', () => {
+	document.querySelector('#asusRef').checked = false
+	document.querySelector('#hpRef').checked = false
+	document.querySelector('#acerRef').checked = false
+	document.querySelector('#dellRef').checked = false
+	document.querySelector('#fourGBRef').checked = false
+	document.querySelector('#eightGBRef').checked = false
+	document.querySelector('#steenGBRef').checked = false
+	document.querySelector('#ttwoGBRef').checked = false
+	document.querySelector('#sfourGBRef').checked = false
+	window.location.href = 'Laptop.php';
+	document.querySelector('#clearFilters').style.display = "none";
+})
+
+function toggleref() {
+	var x = document.querySelector('.refinements');
+	if (x.style.display === "none") {
+		x.style.display = "block";
+		x.style.transform = "translateX(0px)";
+	//   document.querySelector('.responsive-filter').style.transform = "translateY(-30px)";
+	} else {
+		
+		x.style.transform = "translateX(-70px)";
+		x.style.display = "none";
+		// document.querySelector('.responsive-filter').style.transform = "translateY(0px)";
+	}
+}
+
+
+// document.querySelector('#clearFilters').addEventListener('click', () => {
+// 	document.querySelector('#fourGBRef').checked = false
+// 	document.querySelector('#eightGBRef').checked = false
+// 	document.querySelector('#steenGBRef').checked = false
+// 	document.querySelector('#ttwoGBRef').checked = false
+// 	document.querySelector('#sfourGBRef').checked = false
+
+// 	// window.location.href = 'Laptop.php';
+// 	document.querySelector('#clearFilters').style.display = "none";
+// })
