12dae815e38c28ff5db0f5977b9d387f51a02672
Sumesh Mohanty <sumemonty@gmail.com>
2024-01-22 19:11:46 +0530

    chg: Modified Database login credentials

--- components/connect.php
+++ components/connect.php
@@ -1,8 +1,8 @@
 <?php
 
-$db_name = 'mysql:host=localhost;dbname=venus';
-$user_name = 'root';
-$user_password = '';
+$db_name = 'mysql:host=localhost;dbname=u911350761_venus';
+$user_name = 'u911350761_venus_trading';
+$user_password = 'Jamna2tara#';
 
 $conn = new PDO($db_name, $user_name, $user_password);
 
